643a3219ded0dcb3357056cb8e858c6608ee2800
Ashlie Gray <ashliejanegray@gmail.com>
2020-12-01 12:23:53 +1000

    again button active and refreshes

--- script.js
+++ script.js
@@ -10,18 +10,22 @@
 // console.log(document.querySelector(".guess").value)
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1
-document.querySelector(".number").textContent = secretNumber
 let score = 20
 
 document.querySelector(".check").addEventListener("click", function(){
     const guess = Number(document.querySelector(".guess").value)
     console.log(guess)
 
+// when there is no input
     if(!guess) {
         document.querySelector(".message").textContent = "⛔️ No number!"
+// when player wins
     } else if (guess === secretNumber){
         document.querySelector(".message").textContent = "🎉 Correct Number!"
-
+        document.querySelector(".number").textContent = secretNumber
+        document.querySelector("body").style.backgroundColor = "#9b6dff"
+        document.querySelector(".number").style.width = "30rem"
+// when guess is too high
     } else if (guess > secretNumber) {
         if(score > 1) {
         document.querySelector(".message").textContent = "📈 Too high!"
@@ -29,9 +33,10 @@ document.querySelector(".check").addEventListener("click", function(){
         document.querySelector(".score").textContent = score
     } else {
         document.querySelector(".message").textContent = "😢 You Lost the Game"
+        document.querySelector("body").style.backgroundColor = "#ff3d41"
         document.querySelector(".score").textContent = 0    
     }
-
+// when guess is too low
     } else if (guess < secretNumber ) {
         if(score > 1) {
             document.querySelector(".message").textContent = "📉 Too low!"
@@ -43,4 +48,7 @@ document.querySelector(".check").addEventListener("click", function(){
         }
     
     }
+})
+document.querySelector(".again").addEventListener("click", function(){
+    location.reload()
 })
